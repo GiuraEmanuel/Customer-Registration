daf1c0e189ac87dc9dcc1aa23491053222745d14
Giura Emanuel <emanuel1996a@gmail.com>
2023-03-01 22:25:37 +0200

    implement customer view list

--- Source/Custom-Registration.Web/Controllers/CustomerController.cs
+++ Source/Custom-Registration.Web/Controllers/CustomerController.cs
@@ -1,12 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Custom_Registration.CodeFirst;
+using Custom_Registration.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Custom_Registration.Web.Controllers
 {
+    [Route("Customer")]
     public class CustomerController : Controller
     {
-        public IActionResult Index()
+        private readonly AppDbContext _appDbContext;
+
+        public CustomerController(AppDbContext appDbContext)
         {
-            return View();
+            _appDbContext = appDbContext;
+        }
+
+        [HttpGet("Customers")]
+        public async Task<IActionResult> GetAllCustomers()
+        {
+            var customers = await _appDbContext.Customers.Select(c=> new CustomerViewModel.Customer(c.Name,c.Website,c.Email,c.PhoneNumber)).ToListAsync();
+
+            var customerViewModel = new CustomerViewModel(customers);
+
+            return View(customerViewModel);
         }
     }
 }
---
+++ Source/Custom-Registration.Web/Models/CustomerViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Custom_Registration.CodeFirst;
+
+namespace Custom_Registration.Web.Models
+{
+    public class CustomerViewModel
+    {
+        public IEnumerable<Customer> Customers { get; }
+
+        public CustomerViewModel(IEnumerable<Customer> customers)
+        {
+            Customers = customers;
+        }
+
+        public class Customer
+        {
+            public Customer(string name, string website, string email, string phoneNumber)
+            {
+                Name = name;
+                Website = website;
+                Email = email;
+                PhoneNumber = phoneNumber;
+            }
+
+            public string Name { get; set; }
+
+            public string Website { get; set; }
+
+            public string Email { get; set; }
+
+            public string PhoneNumber { get; set; }
+        }
+    }
+}
